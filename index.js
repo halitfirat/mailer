f3c8108169dbe82277739ec3037a788993d3d3c1
halitfirat <mail@halitfirat.de>
2021-03-16 13:02:44 +0100

    Change for automatic deploy test

--- index.js
+++ index.js
@@ -6,5 +6,9 @@ app.get('/', (req, res) => {
   res.send({ youre: 'online' });
 });
 
+app.get('/change', (req, res) => {
+  res.send({ confirm: 'change' });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
