cf0146b45fc2e1408ab4160a0fc5602f0fa9a2c8
halitfirat <mail@halitfirat.de>
2021-03-16 12:58:54 +0100

    Reverted change

--- index.js
+++ index.js
@@ -6,9 +6,5 @@ app.get('/', (req, res) => {
   res.send({ youre: 'online' });
 });
 
-app.get('/change', (req, res) => {
-  res.send({ confirm: 'change' });
-});
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
